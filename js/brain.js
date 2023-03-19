188d3ae3a9c735c7acb17dba62fd353de37805bd
ObadaSizo <114221740+ObadaSizo@users.noreply.github.com>
2023-03-19 19:02:07 +0200

    Add files via upload

--- js/brain.js
+++ js/brain.js
@@ -1,8 +1,8 @@
 let count = 1; 
 let slide = document.getElementById("slide");
 
-document.getElementById("down").onclick =function() {
-    if(count < 4){
+document.getElementById("down").onclick = () => {
+    if(count < 5){
         let sub= document.getElementById(`s${count}`);
         sub.style.animationName="slide-down";
         document.getElementById(`d${count}`).style.animationName="dot-out";
@@ -10,16 +10,16 @@ document.getElementById("down").onclick =function() {
         document.getElementById(`d${count+1}`).style.animationName="dot-in";
         count++;
     } else{count++;}
-    if(count == 5) {
+    if(count == 6) {
         count = 1;
-        let sub= document.getElementById(`s${4}`);
+        let sub= document.getElementById(`s${5}`);
         sub.style.animationName="slide-down";
-        document.getElementById(`d${4}`).style.animationName="dot-out";
+        document.getElementById(`d${5}`).style.animationName="dot-out";
         document.getElementById(`s${1}`).style.animationName="scroll-up";
         document.getElementById(`d${1}`).style.animationName="dot-in";
     }
 };
-document.getElementById("up").onclick =function() {
+document.getElementById("up").onclick = () => {
     if(count>1){
         let sub= document.getElementById(`s${count}`);
         sub.style.animationName="slide-up";
@@ -29,12 +29,40 @@ document.getElementById("up").onclick =function() {
         count--;
     }else{count--;}
     if(count == 0) {
-        count = 4;
+        count = 5;
         let sub= document.getElementById(`s${1}`);
         sub.style.animationName="slide-up";
         document.getElementById(`d${1}`).style.animationName="dot-out";
-        document.getElementById(`s${4}`).style.animationName="scroll-down";
-        document.getElementById(`d${4}`).style.animationName="dot-in";
+        document.getElementById(`s${5}`).style.animationName="scroll-down";
+        document.getElementById(`d${5}`).style.animationName="dot-in";
     }
 };
 
+let dots = document.querySelectorAll(".dot");
+let slides = document.querySelectorAll(".project-card");
+console.log(dots);
+console.log(slides);
+for (let i=0;i<=dots.length-1;i++) {
+    let dot = dots[i];
+    dot.onclick =() => {
+        let curr = count;
+        let clicked;
+        clicked =i+1;
+        if(clicked > curr) {
+            let sub= document.getElementById(`s${curr}`);
+            sub.style.animationName="slide-down";
+            document.getElementById(`d${curr}`).style.animationName="dot-out";
+            document.getElementById(`s${clicked}`).style.animationName="scroll-up";
+            document.getElementById(`d${clicked}`).style.animationName="dot-in";
+            count=clicked;
+        }
+        if(clicked < curr) {
+            let sub= document.getElementById(`s${curr}`);
+            sub.style.animationName="slide-up";
+            document.getElementById(`d${curr}`).style.animationName="dot-out";
+            document.getElementById(`s${clicked}`).style.animationName="scroll-down";
+            document.getElementById(`d${clicked}`).style.animationName="dot-in";
+            count=clicked;
+        }
+    }
+}
